d2397a502123ac23740c3f346055236c32fc93a2
Dmitry <dimarezvyh1@mail.ru>
2026-05-01 18:38:39 +0300

    init: project structure

---
+++ backend/app/core/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_MODE: str
+    APP_PORT: int
+
+    PG_HOST: str
+    PG_PORT: int
+    PG_DATABASE: str
+    PG_USERNAME: str
+    PG_PASSWORD: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASSWORD: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_SERVER: str
+    MAIL_PORT: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
